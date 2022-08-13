bbfef8d54ca220c3962e7fa3441086f91c246e7d
mooz <63669478+mooziii@users.noreply.github.com>
2022-08-13 15:32:35 +0200

    Remove not working suggestions

--- src/main/kotlin/me/obsilabor/commandpassthrough/paper/Paper.kt
+++ src/main/kotlin/me/obsilabor/commandpassthrough/paper/Paper.kt
@@ -10,26 +10,15 @@ import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.plugin.messaging.PluginMessageListener
 
-class Paper : JavaPlugin(), PluginMessageListener, Listener {
+class Paper : JavaPlugin(), PluginMessageListener {
     override fun onEnable() {
         ConfigManager.config
         Bukkit.getMessenger().registerIncomingPluginChannel(this, Common.CHANNEL, this)
-        Bukkit.getPluginManager().registerEvents(this, this)
     }
 
     override fun onPluginMessageReceived(channel: String, player: Player, message: ByteArray?) {
         if (channel == Common.CHANNEL) {
             player.performCommand(message?.decodeToString() ?: return)
-        }
-    }
-
-    @EventHandler
-    fun onPlayerJoin(event: PlayerJoinEvent) {
-        val commands = ConfigManager.config.commandMap
-        commands.keys.forEach {
-            if (Bukkit.getCommandMap().getCommand(it) == null) {
-                event.player.addAdditionalChatCompletions(listOf("/$it"))
-            }
         }
     }
 }
